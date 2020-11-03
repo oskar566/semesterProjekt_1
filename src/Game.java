5c62729f4c4b0a3c07c7bf85be47dcf5b74135c6
Mathias Nielsen <mnhnielsen@gmail.com>
2020-11-03 19:47:34 +0100

    EndRoom Update 1
    First sketch to the end room

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
-public class Game
-{
+public class Game {
     private Parser parser;
     private Room currentRoom;
     private Player player;
-        
+    Scanner input = new Scanner(System.in);
 
-    public Game() 
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
     }
 
 
-    private void createRooms()
-    {
+    private void createRooms() {
 
         Room entry, tutorial, currencyRoom,
                 currencyObtainRoom1, currencyObtainRoom, DesertBaseRoom, Desert1,
@@ -33,9 +30,9 @@ public class Game
 
         DesertBaseRoom = new Room("in the desert base room. Choose a direction to go to a desert", 5);
         Desert1 = new Room("in the first desert. Stop the desertification", 6);
-        Desert2 = new Room("in the second desert. Stop the desertification",6);
-        Desert3 = new Room("in the third desert. Stop the desertification",6);
-        EndRoom = new Room("in the end room. Here is a little test to end the game",7);
+        Desert2 = new Room("in the second desert. Stop the desertification", 6);
+        Desert3 = new Room("in the third desert. Stop the desertification", 6);
+        EndRoom = new Room("in the end room. Here is a little test to end the game", 7);
 
         entry.setExit("north", tutorial);
 
@@ -67,29 +64,26 @@ public class Game
         currentRoom = entry;
     }
 
-    public void play() 
-    {            
+    public void play() {
+        if (currentRoom.getType() == 1) {
+            System.out.println("Enter your name: ");
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+            player = new Player(input.nextLine());
 
-        player = new Player(input.nextLine());
+            System.out.println("Welcome " + player.getName());
 
-        System.out.println("Welcome " + player.getName());
-
-        printWelcome();
-
+            printWelcome();
+        }
 
         boolean finished = false;
-        while (! finished) {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
@@ -98,51 +92,88 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
-    private boolean processCommand(Command command) 
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
+        boolean isAnswered = false;
 
         CommandWord commandWord = command.getCommandWord();
 
-        if(commandWord == CommandWord.UNKNOWN) {
+        if (commandWord == CommandWord.UNKNOWN) {
             System.out.println("I don't know what you mean...");
             return false;
         }
 
         if (commandWord == CommandWord.HELP) {
             printHelp();
-        }
-        else if (commandWord == CommandWord.GO) {
+        } else if (commandWord == CommandWord.GO) {
             goRoom(command);
-        }
-        else if (commandWord == CommandWord.QUIT) {
+        } else if (commandWord == CommandWord.QUIT) {
             wantToQuit = quit(command);
-        }
-        else if (commandWord == CommandWord.PICKUP) {
-            if(currentRoom.getType() == 4){
+        } else if (commandWord == CommandWord.PICKUP) {
+            if (currentRoom.getType() == 4) {
                 player.addTrash();
                 currentRoom.removeTrash();
             }
-        }
-        else if(commandWord == CommandWord.SELL && currentRoom.getType() == 3){
-            if(player.hasTrash()){
+        } else if (commandWord == CommandWord.SELL && currentRoom.getType() == 3) {
+            if (player.hasTrash()) {
                 player.sellTrash();
             }
-        }
-        else if(commandWord == CommandWord.BUY && currentRoom.getType()==3){
-            if(player.getCoins()>0){
+        } else if (commandWord == CommandWord.BUY && currentRoom.getType() == 3) {
+            if (player.getCoins() > 0) {
                 player.addSapling();
             }
         }
 
-        if (currentRoom.getType() == 7){
-            System.out.println("ENDROOM");
+        if (currentRoom.getType() == 7) {
+            boolean question1 = false, question2 = false, question3 = false;
+
+            System.out.println("You are almost finished. You need to answer the following questions correctly");
+            System.out.println("Question 1: What is 1+1");
+
+            while (!question1) {
+                if (input.nextLine().equals("2")) {
+                    System.out.println("Correct");
+                    question1 = true;
+                } else {
+                    System.out.println("Try again, Here are some hints");
+                    System.out.println("3    4    2");
+                    //question1 = false;
+                }
+            }
+            System.out.println("Here is another question");
+            System.out.println("Question 2: What is 2 + 2?");
+            while (!question2) {
+                if (input.nextLine().equals("4")) {
+                    System.out.println("Correct");
+                    question2 = true;
+                } else {
+                    System.out.println("Try again, Here are some hints");
+                    System.out.println("5    4    9");
+                    //question2 = false;
+                }
+            }
+            System.out.println("Here is another question");
+            System.out.println("Question 2: What is 2 * 6?");
+            while (!question3) {
+                if (input.nextLine().equals("12")) {
+                    System.out.println("Correct. Thanks for playing");
+                    question3 = true;
+                    isAnswered = true;
+                } else {
+                    System.out.println("Try again, Here are some hints");
+                    System.out.println("15    14    12");
+                    //question2 = false;
+                }
+            }
         }
+        if(isAnswered){
+            System.exit(0);
+        }
+
         return wantToQuit;
     }
 
-    private void printHelp() 
-    {
+    private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
         System.out.println();
@@ -150,9 +181,8 @@ public class Game
         parser.showCommands();
     }
 
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Go where?");
             return;
         }
@@ -163,20 +193,17 @@ public class Game
 
         if (nextRoom == null) {
             System.out.println("There is no door!");
-        }
-        else {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
         }
     }
 
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
