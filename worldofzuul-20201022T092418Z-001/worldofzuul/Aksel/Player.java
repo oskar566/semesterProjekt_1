123112d0c083ad43f40ce020561d35fcee7cbe39
Akselthyregod <59265356+Akselthyregod@users.noreply.github.com>
2020-11-03 09:45:36 +0100

    Test Aksel

---
+++ worldofzuul-20201022T092418Z-001/worldofzuul/Aksel/Player.java
@@ -0,0 +1,41 @@
+package Aksel;
+
+import worldofzuul.Coin;
+
+public class Player {
+
+    private static int coins;
+    private static String name;
+    private static Inventory inventory;
+    private static Coin coin;
+
+
+
+
+    public Player(String name){
+
+        inventory = new Inventory();
+        coin = new Coin();
+        this.name = name;
+
+    }
+
+
+    public void waterSapling(){
+
+    }
+
+    public void plantTree(){
+       inventory.removeSapling();
+
+    }
+
+    public void sellTrash(){
+        inventory.removeTrash();
+        coin.addCoin();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
