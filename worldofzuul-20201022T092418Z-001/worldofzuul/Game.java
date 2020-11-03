7c619a70ccad94387fcfbff52296051fe235a8a9
Mathias Nielsen <mnhnielsen@gmail.com>
2020-11-03 10:02:26 +0100

    Changes to the game room

--- worldofzuul-20201022T092418Z-001/worldofzuul/Game.java
+++ worldofzuul-20201022T092418Z-001/worldofzuul/Game.java
@@ -19,6 +19,7 @@ public class Game
 //test2
     private void createRooms()
     {
+        /*
         Room outside, theatre, pub, lab, office;
       
         outside = new Room("outside the main entrance of the university");
@@ -41,6 +42,48 @@ public class Game
         office.setExit("west", lab);
 
         currentRoom = outside;
+
+         */
+        Room entry, tutorial, currencyRoom,
+                currencyObtainRoom1, currencyObtainRoom, DesertBaseRoom, Desert1,
+                Desert2, Desert3, EndRoom;
+
+        entry = new Room("at the entry room. Here you can find information on desertification.");
+        tutorial = new Room(" in the tutorial room. Here you can learn how to play the game.");
+        currencyRoom = new Room("in the currency room. Here you can exchange your trash for saplings.");
+        currencyObtainRoom = new Room("in the currency obtain room. Here you can harvest trash.");
+        currencyObtainRoom1 = new Room("currency obtain room. Here you can harvest trash.");
+        DesertBaseRoom = new Room("in the desert base room. Choose a direction to go to a desert");
+        Desert1 = new Room("in the first desert. Stop the desertification");
+        Desert2 = new Room("in the second desert. Stop the desertification");
+        Desert3 = new Room("in the third desert. Stop the desertification");
+        EndRoom = new Room("in the end room. Here is a little test to end the game");
+
+        entry.setExit("north", tutorial);
+
+        tutorial.setExit("north", currencyRoom);
+        tutorial.setExit("south", tutorial);
+
+        currencyRoom.setExit("north", DesertBaseRoom);
+        currencyRoom.setExit("west", currencyObtainRoom);
+        currencyRoom.setExit("east", currencyObtainRoom1);
+        currencyRoom.setExit("south", tutorial);
+
+        currencyObtainRoom1.setExit("west",currencyRoom);
+
+        currencyObtainRoom.setExit("east", currencyRoom);
+
+        DesertBaseRoom.setExit("north",Desert3);
+        DesertBaseRoom.setExit("west", Desert1);
+        DesertBaseRoom.setExit("east", Desert2);
+        DesertBaseRoom.setExit("south", currencyRoom);
+
+        Desert1.setExit("east", DesertBaseRoom);
+
+        Desert2.setExit("west", DesertBaseRoom);
+
+        Desert3.setExit("north", EndRoom);
+        Desert3.setExit("south", DesertBaseRoom);
     }
 
     public void play() 
@@ -59,8 +102,8 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Welcome to the Desertification game!");
+        System.out.println("Desertification is a serious problem");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
