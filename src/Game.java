db61d62cefeb8677e04ae6d179b9cba3723ef268
Mathias Nielsen <mnhnielsen@gmail.com>
2020-11-05 10:05:40 +0100

    Endroom updatte

--- src/Game.java
+++ src/Game.java
@@ -37,7 +37,7 @@ public class Game {
         entry.setExit("north", tutorial);
 
         tutorial.setExit("north", currencyRoom);
-        tutorial.setExit("south", tutorial);
+        //tutorial.setExit("south", tutorial);
 
         currencyRoom.setExit("north", DesertBaseRoom);
         currencyRoom.setExit("west", currencyObtainRoom);
